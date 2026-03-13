058756b27e64edc678234c7f62d046e200bbebba
Carlo Camilloni <carlo.camilloni@gmail.com>
2026-03-13 09:53:31 +0100

    config tests

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+ 
 export default defineConfig({
   plugins: [react()],
-  base: '/Aging/',
+  base: '/aging/',
+  test: {
+    environment: 'node',
+    include: ['tests/**/*.test.js'],
+  },
 })
